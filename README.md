11f3a333a8b274f8c97e0b484bac320f082bece9
Lília Paula <liliaxtz2@gmail.com>
2021-04-11 22:46:48 -0300

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+
+<h3 align="center"> Projeto: Aplicativo de Assinatura </h3>
+
+<!-- <h2 align="center"></h2>
+
+<h3 align="center">
+  
+</h3> -->
+
+## Techs
+
+- NextJS
+- TypeScript
+- Stripe
+- Fauna DB
+- SASS
+
+<!-- <h3 align="center">
+    APP: <a href="#" target="_blank">Visualizar aplicação<a>
+</h3> -->
+
+<p align="center"> Developed by 🍄 Lília Paula </p>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SignInButton } from '../SingInButton'
+import styles from './styles.module.scss'
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="Signature"/>
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
